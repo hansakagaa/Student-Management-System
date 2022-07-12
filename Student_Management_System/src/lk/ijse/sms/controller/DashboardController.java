d62505ad338bc5cf53c2d3510cbdf6c79b92f5d5
Hasii-boy <hansakagaa@gmail.com>
2022-07-12 11:47:16 +0530

    Dashboard controller Student Initial

--- Student_Management_System/src/lk/ijse/sms/controller/DashboardController.java
+++ Student_Management_System/src/lk/ijse/sms/controller/DashboardController.java
@@ -1,7 +1,27 @@
 package lk.ijse.sms.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 /**
  * @author : hansakagaa
  **/
 public class DashboardController {
+    public JFXTextField StudentId;
+    public JFXTextField StudentName;
+    public JFXTextField StudentEmail;
+    public JFXTextField StudentContact;
+    public JFXTextField StuAddress;
+    public JFXTextField StudentNIC;
+    public JFXButton btnStudent;
+    public JFXButton btnStudentDelete;
+
+    public void studentSaveOrUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void studentDeleteOnAction(ActionEvent actionEvent) {
+
+    }
 }
